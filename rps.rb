85364f5c46029a291e598dece631819adff193e2
Mauro Meden <mauro.meden@shopify.com>
2021-06-20 09:35:24 -0600

    Add basic output based on users' input

--- rps.rb
+++ rps.rb
@@ -12,3 +12,32 @@ print "Player two - Enter your selection: "
 player_two = gets.chomp
 
 # Print outcome of game
+case player_one
+when "Rock"
+    case player_two
+    when "Rock"
+        puts "Tie!"
+    when "Paper"
+        puts "Paper covers rock"
+    when "Scissors"
+        puts "Rock crushes scissors"
+    end
+when "Paper"
+    case player_two
+    when "Rock"
+        puts "Paper covers rock"
+    when "Paper"
+        puts "Tie!"
+    when "Scissors"
+        puts "Scissors cut paper"
+    end
+when "Scissors"
+    case player_two
+    when "Rock"
+        puts "Rock crushes scissors"
+    when "Paper"
+        puts "Scissors cut paper"
+    when "Scissors"
+        puts "Tie!"
+    end
+end
