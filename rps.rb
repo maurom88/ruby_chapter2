99f53f409653c26e113206712ba1e51327605be3
Mauro Meden <mauro.meden@shopify.com>
2021-06-20 10:25:16 -0600

    Repeat game in case of a tie

--- rps.rb
+++ rps.rb
@@ -1,67 +1,86 @@
 # Declare two empty variables to store the players' selections
 player_one = ""
 player_two = ""
+
+# Set variable to determine whether the user entered a valid input or not
 accepted_value = false
 
-
-# Get first input
-until accepted_value == true
-    print "Player one - Enter your selection: "
-    player_one = gets.chomp.capitalize
-    case player_one
-    when "Rock", "Paper", "Scissors"
-        accepted_value = true
-    else
-        puts "No cheaters! Only rock, paper, scissors allowed"
-    end
-end
-
-accepted_value = false
-# Get second input
-until accepted_value == true
-    print "Player two - Enter your selection: "
-    player_two = gets.chomp.capitalize
-    case player_two
-    when "Rock", "Paper", "Scissors"
-        accepted_value = true
-    else
-        puts "No cheaters! Only rock, paper, scissors allowed"
-    end
-end
+# Set a variable to determint whether to run the game again or not
+# Always runs the game at least once
+play_again = true
 
 # Print outcome of game
-case player_one
-when "Rock"
-    case player_two
-    when "Rock"
-        puts "Tie!"
-    when "Paper"
-        puts "Paper covers rock"
-        puts "Player two wins"
-    when "Scissors"
-        puts "Rock crushes scissors"
-        puts "Player one wins"
+while play_again == true
+
+    # Get first input
+    until accepted_value == true
+        print "Player one - Enter your selection: "
+        player_one = gets.chomp.capitalize
+        case player_one
+        when "Rock", "Paper", "Scissors"
+            accepted_value = true
+        else
+            puts "No cheaters! Only rock, paper, scissors allowed"
+        end
     end
-when "Paper"
-    case player_two
-    when "Rock"
-        puts "Paper covers rock"
-        puts "Player one wins"
-    when "Paper"
-        puts "Tie!"
-    when "Scissors"
-        puts "Scissors cut paper"
-        puts "Player two wins"
+
+    # Reset control variable for accepted input from user
+    accepted_value = false
+
+    # Get second input
+    until accepted_value == true
+        print "Player two - Enter your selection: "
+        player_two = gets.chomp.capitalize
+        case player_two
+        when "Rock", "Paper", "Scissors"
+            accepted_value = true
+        else
+            puts "No cheaters! Only rock, paper, scissors allowed"
+        end
     end
-when "Scissors"
-    case player_two
+
+    # Reset control variable for accepted input from user
+    accepted_value = false
+
+    case player_one
     when "Rock"
-        puts "Rock crushes scissors"
-        puts "Player two wins"
+        case player_two
+        when "Rock"
+            puts "Tie!"
+        when "Paper"
+            puts "Paper covers rock"
+            puts "Player two wins"
+            play_again = false
+        when "Scissors"
+            puts "Rock crushes scissors"
+            puts "Player one wins"
+            play_again = false
+        end
     when "Paper"
-        puts "Scissors cut paper"
-        puts "Player one wins"
+        case player_two
+        when "Rock"
+            puts "Paper covers rock"
+            puts "Player one wins"
+            play_again = false
+        when "Paper"
+            puts "Tie!"
+        when "Scissors"
+            puts "Scissors cut paper"
+            puts "Player two wins"
+            play_again = false
+        end
     when "Scissors"
-        puts "Tie!"
+        case player_two
+        when "Rock"
+            puts "Rock crushes scissors"
+            puts "Player two wins"
+            play_again = false
+        when "Paper"
+            puts "Scissors cut paper"
+            puts "Player one wins"
+            play_again = false
+        when "Scissors"
+            puts "Tie!"
+        end
     end
 end
