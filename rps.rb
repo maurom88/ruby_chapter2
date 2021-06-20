e30791ee147f1c98a033385fd23bc29087c27a7e
Mauro Meden <mauro.meden@shopify.com>
2021-06-20 09:29:45 -0600

    Create file

---
+++ rps.rb
@@ -0,0 +1,14 @@
+# Declare two empty variables to store the players' selections
+player_one = ""
+player_two = ""
+
+
+# Get first input
+print "Player one - Enter your selection: "
+player_one = gets.chomp
+
+# Get second input
+print "Player two - Enter your selection: "
+player_two = gets.chomp
+
+# Print outcome of game
